4259477353f4d9e3b16cd24d9a1c13b4f1a85a0b
AnasSaifeee <AnasSaifeee@anas.saifi@iic.ac.in>
2022-10-25 03:31:47 +0530

    committ😭😭

--- backend/index.js
+++ backend/index.js
@@ -11,56 +11,92 @@ app.use(express.json())
 var db = mysql.createConnection({
     host:'localhost',
     user:'root',
-    password:'root123',
+    password:'123456',
     database:"e_waste",
-    port: '3306'
+    port:'3306'
+   
 });
 
 
-app.post("/signup", (req, res)=>{
+// app.post("/signup", (req, res)=>{
+//     const name = req.body.name;
+//     const username = req.body.username;
+//     const email = req.body.email;
+//     const role = req.body.role;
+//     const password = req.body.password;
+//     const confirmpassword = req.body.confirmpassword;
+    
+//     db.query(
+//         "INSERT INTO signup_data (name, username, email, role, password, confirmpassword) VALUES (?,?,?,?,?,?)", 
+//         [ name, username, email, role, password, confirmpassword],
+//         (err, result) => {
+//             console.log(err);
+//         }
+
+//     );
+
+//     db.query("DELETE FROM signup_data WHERE id NOT IN (SELECT MIN(id) FROM signup_data GROUP BY name,username,email,role, password, confirmpassword)");
+    
+// });
+
+// app.post('/signin', (req, res)=>{
+//     const email = req.body.email;
+//     const password = req.body.password;
+    
+//     db.query(
+//         "SELECT * FROM signup_data WHERE email = ? AND password = ?", 
+//         [ email, password],
+//         (err, result) => {
+            
+//             if (err){
+//                 res.send({err: err});
+//             }
+
+//             if (result.length > 0){
+//                 res.send(result)
+//             } else {
+//                 res.send({message: "Wrong Email/Password combination!"})
+//             }
+            
+//         }
+
+//     );
+// });
+
+app.post('/request',(req,res)=>
+{
     const name = req.body.name;
-    const username = req.body.username;
     const email = req.body.email;
-    const role = req.body.role;
-    const password = req.body.password;
-    const confirmpassword = req.body.confirmpassword;
+    const address = req.body.address;
+    const district = req.body.district;
+    const city = req.body.city;
+    const state = req.body.state;
+    const pincode = req.body.pincode;
+    const contact_number = req.body.contact;
+    const date = req.body.date;
+    const time = req.body.time;
+    const ename = req.body.ename;
+    const equantity = req.body.equantity;
+    const type = req.body.type;
+    const weight = req.body.weight;
+    const img1 = req.body.img1;
+    const img2 = req.body.img2;
+    const img3 = req.body.img3;
+    const comment= req.body.comment;
     
     db.query(
-        "INSERT INTO signup_data (name, username, email, role, password, confirmpassword) VALUES (?,?,?,?,?,?)", 
-        [ name, username, email, role, password, confirmpassword],
-        (err, result) => {
-            console.log(err);
-        }
-
-    );
-
-    db.query("DELETE FROM signup_data WHERE id NOT IN (SELECT MIN(id) FROM signup_data GROUP BY name,username,email,role, password, confirmpassword)");
+                "INSERT INTO request (name, email, address, district, city, state,pincode,contact_number,date,time,ename,equantity,type,weight,img1,img2,img3,comment) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)", 
+                [name, email, address, district, city, state,pincode,contact_number,date,time,ename,equantity,type,weight,img1,img2,img3,comment],
+                (err, result) => {
+                    console.log(err);
+                }
+        
+            );
     
-});
+   console.log(req.body);
+   res.json({status:'ok'})
 
-app.post('/signin', (req, res)=>{
-    const email = req.body.email;
-    const password = req.body.password;
-    
-    db.query(
-        "SELECT * FROM signup_data WHERE email = ? AND password = ?", 
-        [ email, password],
-        (err, result) => {
-            
-            if (err){
-                res.send({err: err});
-            }
-
-            if (result.length > 0){
-                res.send(result)
-            } else {
-                res.send({message: "Wrong Email/Password combination!"})
-            }
-            
-        }
-
-    );
-});
+})
 
 
 app.listen(4000, () => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Route,Routes  } from 'react-router-dom';
+import Home from './pages/Home';
+import Notifications from './pages/Notifications';
+import Schedule from './pages/Schedule';
 
-import Home from "./Home";
-import Signin from "./signin";
-import Signup from './signup';
 function App() {
   return (
-   <>
-<Signin/>
-   </>
-   
+    <>
+    <BrowserRouter>
+           <Navbar/>
+      <Routes>
+          <Route path='/' exact element={<Home/>} />
+          <Route path='/notifications' element={<Notifications/>} />
+          <Route path='/schedule' element={<Schedule/>} />
+          
+      </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
